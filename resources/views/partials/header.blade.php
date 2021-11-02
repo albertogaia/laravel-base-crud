7f5f485d49376fcd5f6947202c74241f3f802a3b
albertogaia <alberto.gaia95@gmail.com>
2021-11-02 16:20:13 +0100

    Creazione nuovo fumetto e redirect

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -13,7 +13,7 @@
             <a class="nav-link active" href="{{route('comics.index')}}">Lista fumetti</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Inserisci un fumetto</a>
+            <a class="nav-link" href="{{route('comics.create')}}">Inserisci un fumetto</a>
           </li>
         </ul>
       </div>
