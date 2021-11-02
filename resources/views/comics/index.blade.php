35d5a8b55ee0082bcc576e0de8d3938dcdd1f923
albertogaia <alberto.gaia95@gmail.com>
2021-11-02 15:39:59 +0100

    Stampato in tabella i fumetti

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,6 +1,31 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <h1>sono l'indice dei fumetti</h1>  
+        <div class="row">
+            <div class="col-12">
+                <h1>Tutti i fumetti</h1>
+                <table class="table table-striped">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Type</th>
+                        <th scope="col">Price</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($comics as $comic)
+                            <tr>
+                                <th scope="row">{{$comic['id']}}</th>
+                                <td>{{$comic['title']}}</td>
+                                <td>{{$comic['type']}}</td>
+                                <td>{{$comic['price']}} €</td>
+                            </tr>
+                            
+                        @endforeach
+                    </tbody>
+                  </table>
+            </div>
+        </div>
     </div>
 @endsection
